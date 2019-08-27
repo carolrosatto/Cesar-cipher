f8232dc54ee0889ba6bc81a5193b04fc97d0ed26
Carolina Rosatto <carolrosatto@gmail.com>
2019-08-27 12:09:41 -0300

    JS funcionando e CSS finalizado

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@
 document.getElementById("encodebutton").addEventListener ("click", function encodeBtnFunction() {
   let encodeWord = document.getElementById("encodetext").value;
   let key = parseInt(document.getElementById("cipherkey").value);
-  document.getElementById("etext").innerHTML = window.cipher.encode(encodeWord, key);
+  document.getElementById("etext").innerHTML = window.cipher.encode(key, encodeWord);
 });
 
 document.getElementById("decodebutton").addEventListener ("click", function decodeBtnFunction() {
   let decodeWord = document.getElementById("decodetext").value;
   let key = parseInt(document.getElementById("cipherkey").value);
   
-  document.getElementById("dtext").innerHTML = window.cipher.decode(decodeWord, key);
+  document.getElementById("dtext").innerHTML = window.cipher.decode(key, decodeWord);
     
 });
